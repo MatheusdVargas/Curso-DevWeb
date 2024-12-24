8bebab775cc23cd7443498e03043c4e85a654e6f
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2024-12-24 07:55:33 -0300

    Add files via upload

---
+++ Modulo01/ModuloD/ex0011b.js
@@ -0,0 +1,21 @@
+/*
+var idade = 22;
+if (idade < 16) {
+  console.log("Nao vota");
+} else {
+  if (idade >= 16 && idade < 18) {
+    console.log("Voto opcional");
+  } else {
+    console.log("Voto obrigatorio");
+  }
+}
+*/
+
+var idade = 22;
+if (idade < 16) {
+  console.log("Nao vota");
+} else if ((idade >= 16 && idade < 18) || idade > 65) {
+  console.log("Voto opcional");
+} else {
+  console.log("Voto obrigatorio");
+}
