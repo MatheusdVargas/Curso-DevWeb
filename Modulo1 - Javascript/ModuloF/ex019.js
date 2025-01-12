b034ceddc24d60d4a0b0dc1304108378ca7665f9
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2025-01-12 20:53:55 -0300

    Add files via upload

---
+++ Modulo1 - Javascript/ModuloF/ex019.js
@@ -0,0 +1,12 @@
+function parOuImpar(num) {
+    if(num % 2 == 0 ) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+    
+}
+let num = parOuImpar(76)
+console.log(num)
+num = parOuImpar(81)
+console.log(num)
