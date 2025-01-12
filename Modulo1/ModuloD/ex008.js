f112a67573ed05b7acfa093ed315df1095e6b82e
Matheus de Vargas <164496759+MatheusdVargas@users.noreply.github.com>
2025-01-12 20:52:42 -0300

    Delete Modulo1 directory

--- Modulo1/ModuloD/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-// Condição Simples
-if (vel > 60) {
-    console.log('Você foi multado!')
-}
--- Modulo1/ModuloF/ex20.js
+++
@@ -1,6 +0,0 @@
-function soma(a, b) {
-    return a + b
-}
-let a = 8
-let b = 2
-console.log(soma(a, b))
